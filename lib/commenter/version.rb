0215ac9d93740fdd4937a90013b81b100efd1770
brooke <brooke@redalchemist.com>
2010-12-20 21:53:04 -0700

    Switched to Bundler for building gems.

---
+++ lib/commenter/version.rb
@@ -0,0 +1,3 @@
+module Commenter
+  VERSION = "1.1.0"
+end
--- spec/acts_as_commentable_spec.rb
+++
@@ -1,7 +0,0 @@
-#require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
-require File.join(File.dirname(__FILE__), "spec_helper.rb")
-
-describe "ActsAsCommentable" do
-  it "should be testable" do
-  end
-end
